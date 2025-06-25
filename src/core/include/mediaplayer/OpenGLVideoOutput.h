5236dd100298cdad290334aab2f1d862ac9dba34
Ram A <102399773+letapicode@users.noreply.github.com>
2025-06-24 21:01:42 -0400

    Add OpenGL video output

---
+++ src/core/include/mediaplayer/OpenGLVideoOutput.h
@@ -0,0 +1,27 @@
+#ifndef MEDIAPLAYER_OPENGLVIDEOOUTPUT_H
+#define MEDIAPLAYER_OPENGLVIDEOOUTPUT_H
+
+#include "VideoOutput.h"
+#include <GLFW/glfw3.h>
+
+namespace mediaplayer {
+
+class OpenGLVideoOutput : public VideoOutput {
+public:
+  OpenGLVideoOutput();
+  ~OpenGLVideoOutput() override;
+
+  bool init(int width, int height) override;
+  void shutdown() override;
+  void displayFrame(const uint8_t *rgba, int linesize) override;
+
+private:
+  GLFWwindow *m_window{nullptr};
+  unsigned int m_texture{0};
+  int m_width{0};
+  int m_height{0};
+};
+
+} // namespace mediaplayer
+
+#endif // MEDIAPLAYER_OPENGLVIDEOOUTPUT_H
