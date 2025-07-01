a38e6ca48e98a01638b0ce4f83af0ab4f7eb0802
Ram A <102399773+letapicode@users.noreply.github.com>
2025-06-30 22:11:59 -0400

    Implement frame pool and optimization settings

---
+++ src/core/include/mediaplayer/FramePool.h
@@ -0,0 +1,29 @@
+#ifndef MEDIAPLAYER_FRAMEPOOL_H
+#define MEDIAPLAYER_FRAMEPOOL_H
+
+#include "VideoFrame.h"
+#include <mutex>
+#include <vector>
+
+namespace mediaplayer {
+
+class FramePool {
+public:
+  explicit FramePool(size_t maxFrames = 5);
+  ~FramePool();
+
+  VideoFrame *acquire(int width, int height, const int linesize[3]);
+  void release(VideoFrame *frame);
+  void clear();
+
+private:
+  VideoFrame *createFrame(int width, int height, const int linesize[3]);
+
+  size_t m_maxFrames;
+  std::vector<VideoFrame *> m_freeFrames;
+  std::mutex m_mutex;
+};
+
+} // namespace mediaplayer
+
+#endif // MEDIAPLAYER_FRAMEPOOL_H
