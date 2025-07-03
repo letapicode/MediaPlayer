79616bd1068545c0bb7e1b585df3f83efd27a43b
Ram A <102399773+letapicode@users.noreply.github.com>
2025-07-02 21:46:00 -0400

    Rewrite LibraryQt README

---
+++ src/desktop/LibraryQt.h
@@ -0,0 +1,42 @@
+#ifndef MEDIAPLAYER_LIBRARYQT_H
+#define MEDIAPLAYER_LIBRARYQT_H
+
+#include "mediaplayer/LibraryDB.h"
+#include "mediaplayer/LibraryScanner.h"
+#include <QObject>
+#include <memory>
+#include <thread>
+
+namespace mediaplayer {
+
+class LibraryQt : public QObject {
+  Q_OBJECT
+public:
+  explicit LibraryQt(QObject *parent = nullptr);
+  ~LibraryQt();
+
+  void setLibrary(LibraryDB *db);
+
+  Q_INVOKABLE void startScan(const QString &directory, bool cleanup = true);
+  Q_INVOKABLE void cancelScan();
+  Q_INVOKABLE bool scanRunning() const;
+  Q_INVOKABLE int current() const;
+  Q_INVOKABLE int total() const;
+
+  Q_INVOKABLE QList<QVariantMap> allMedia() const;
+  Q_INVOKABLE QStringList allPlaylists() const;
+  Q_INVOKABLE QList<QVariantMap> playlistItems(const QString &name) const;
+
+signals:
+  void scanProgress(int current, int total);
+  void scanFinished();
+
+private:
+  LibraryDB *m_db{nullptr};
+  std::unique_ptr<LibraryScanner> m_scanner;
+  std::thread m_waitThread;
+};
+
+} // namespace mediaplayer
+
+#endif // MEDIAPLAYER_LIBRARYQT_H
