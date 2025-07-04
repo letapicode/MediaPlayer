bea7e5a74ec23e3dabce8e4fa4d1a0101411b6f4
Ram A <102399773+letapicode@users.noreply.github.com>
2025-07-04 10:15:23 -0400

    Add MediaPlayer core linkage and simple usage

--- src/desktop/app/main.cpp
+++ src/desktop/app/main.cpp
@@ -1,9 +1,12 @@
+#include "mediaplayer/MediaPlayer.h"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
   QQmlApplicationEngine engine;
+  mediaplayer::MediaPlayer player;
+  player.setVolume(1.0);
   const QUrl url = QUrl::fromLocalFile("qml/Main.qml");
   engine.load(url);
   if (engine.rootObjects().isEmpty())
