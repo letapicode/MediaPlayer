e3baeb084b9be9165e598227d64473e569db32db
Ram A <102399773+letapicode@users.noreply.github.com>
2025-07-04 12:40:27 -0400

    Add macOS integration for media keys and NowPlaying

--- src/desktop/app/macos/MacIntegration.mm
+++ src/desktop/app/macos/MacIntegration.mm
@@ -1,5 +1,81 @@
 #import <Cocoa/Cocoa.h>
-// Placeholder for macOS media key integration
-void setupMacIntegration() {
-  // TODO: implement macOS Now Playing Center hooks
+#import <MediaPlayer/MediaPlayer.h>
+
+#include <QGuiApplication>
+#include <QKeyEvent>
+
+#include "mediaplayer/MediaMetadata.h"
+
+using namespace mediaplayer;
+
+static CFMachPortRef sEventTap = nullptr;
+
+static CGEventRef mediaKeyCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef event,
+                                   void *refcon) {
+  if (type != NX_SYSDEFINED)
+    return event;
+  NSEvent *nsEvent = [NSEvent eventWithCGEvent:event];
+  if ([nsEvent subtype] != NX_SUBTYPE_AUX_CONTROL_BUTTONS)
+    return event;
+
+  int keyCode = ([nsEvent data1] & 0xFFFF0000) >> 16;
+  int keyFlags = ([nsEvent data1] & 0x0000FFFF);
+  bool keyDown = ((keyFlags & 0xFF00) >> 8) == 0xA;
+  if (!keyDown)
+    return event;
+
+  int qtKey = 0;
+  switch (keyCode) {
+  case NX_KEYTYPE_PLAY:
+    qtKey = Qt::Key_MediaPlay;
+    break;
+  case NX_KEYTYPE_FAST:
+    qtKey = Qt::Key_MediaNext;
+    break;
+  case NX_KEYTYPE_REWIND:
+    qtKey = Qt::Key_MediaPrevious;
+    break;
+  default:
+    return event;
+  }
+
+  QWindow *window = QGuiApplication::focusWindow();
+  if (!window && !QGuiApplication::allWindows().isEmpty())
+    window = QGuiApplication::allWindows().first();
+  if (window) {
+    QKeyEvent *press = new QKeyEvent(QEvent::KeyPress, qtKey, Qt::NoModifier);
+    QCoreApplication::postEvent(window, press);
+    QKeyEvent *release = new QKeyEvent(QEvent::KeyRelease, qtKey, Qt::NoModifier);
+    QCoreApplication::postEvent(window, release);
+  }
+  return nullptr;
 }
+
+static void setupMediaKeyTap() {
+  if (sEventTap)
+    return;
+  CGEventMask mask = CGEventMaskBit(NX_SYSDEFINED);
+  sEventTap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0, mask, mediaKeyCallback,
+                               nullptr);
+  if (!sEventTap)
+    return;
+  CFRunLoopSourceRef source = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, sEventTap, 0);
+  CFRunLoopAddSource(CFRunLoopGetMain(), source, kCFRunLoopCommonModes);
+  CFRelease(source);
+  CGEventTapEnable(sEventTap, true);
+}
+
+void updateNowPlayingInfo(const MediaMetadata &meta) {
+  NSMutableDictionary *info = [NSMutableDictionary dictionary];
+  if (!meta.title.empty())
+    info[MPMediaItemPropertyTitle] = [NSString stringWithUTF8String:meta.title.c_str()];
+  if (!meta.artist.empty())
+    info[MPMediaItemPropertyArtist] = [NSString stringWithUTF8String:meta.artist.c_str()];
+  if (!meta.album.empty())
+    info[MPMediaItemPropertyAlbumTitle] = [NSString stringWithUTF8String:meta.album.c_str()];
+  if (meta.duration > 0.0)
+    info[MPMediaItemPropertyPlaybackDuration] = @(meta.duration);
+  [[MPNowPlayingInfoCenter defaultCenter] setNowPlayingInfo:info];
+}
+
+void setupMacIntegration() { setupMediaKeyTap(); }
--- src/desktop/app/main.cpp
+++ src/desktop/app/main.cpp
@@ -5,12 +5,19 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QTranslator>
+#ifdef Q_OS_MAC
+void setupMacIntegration();
+#endif
 
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
   QTranslator translator;
   translator.load("player_en", "translations");
   app.installTranslator(&translator);
+
+#ifdef Q_OS_MAC
+  setupMacIntegration();
+#endif
 
   QQmlApplicationEngine engine;
   mediaplayer::MediaPlayerController controller;
