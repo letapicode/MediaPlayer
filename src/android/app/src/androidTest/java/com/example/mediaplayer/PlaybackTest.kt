51f0e47d4a966fe5a9e3ae234b45685a58c84c0c
Ram A <102399773+letapicode@users.noreply.github.com>
2025-07-06 18:35:38 -0400

    Add Espresso test for library playback

--- src/android/app/src/androidTest/java/com/example/mediaplayer/PlaybackTest.kt
+++ src/android/app/src/androidTest/java/com/example/mediaplayer/PlaybackTest.kt
@@ -1,8 +1,16 @@
 package com.example.mediaplayer
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.rule.ActivityTestRule
+import androidx.test.rule.GrantPermissionRule
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import com.example.mediaplayer.PlaybackService
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,11 +18,23 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class PlaybackTest {
     @get:Rule
-    val rule = ActivityTestRule(MainActivity::class.java)
+    val intentsRule = IntentsTestRule(MainActivity::class.java)
+
+    @get:Rule
+    val permissionRule: GrantPermissionRule =
+        GrantPermissionRule.grant(android.Manifest.permission.READ_EXTERNAL_STORAGE)
 
     @Test
     fun startPlayback() {
         // Simple smoke test ensuring activity launches
         InstrumentationRegistry.getInstrumentation().waitForIdleSync()
+    }
+
+    @Test
+    fun tapLibraryItemStartsService() {
+        onView(withId(R.id.list)).perform(
+            actionOnItemAtPosition<androidx.recyclerview.widget.RecyclerView.ViewHolder>(0, click())
+        )
+        intended(hasComponent(PlaybackService::class.java.name))
     }
 }
